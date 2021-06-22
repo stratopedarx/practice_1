f7e4e0299e700e0aeac5d295e22287231d4430fc
Sergey Lobanov <stratopedarx.lobanov@yandex.ru>
2021-06-22 18:39:29 +0200

    Initial Commit

---
+++ practice_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice_1
+//
+//  Created by user199993 on 6/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
