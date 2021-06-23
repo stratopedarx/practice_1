249e744249f551451913c826ca0fd82c2431c523
Sergey Lobanov <stratopedarx.lobanov@yandex.ru>
2021-06-23 12:04:34 +0200

    Make the skeleton of the task

--- practice_1/ViewController.swift
+++ practice_1/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var a: Int!  // random number from computer
+    var b: Int!  // current number from user
+    var startOfRange = 0
+    var endOfRange = 100
+    @IBOutlet weak var mainMessageLabel: UILabel!
+    @IBOutlet weak var numberFromUserTextField: UITextField!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBAction func getNumberFromUserAction(_ sender: UIButton) {
+        if let numberFromUser = Int(numberFromUserTextField.text!) {
+            if a == nil {
+                a = RandomNumber.getRandomNumber(startOfRange, endOfRange)
+            }
+            if numberFromUser > a {
+                mainMessageLabel.text = "Too much"
+            } else if numberFromUser < a {
+                mainMessageLabel.text = "Too little"
+            } else {
+                mainMessageLabel.text = "Victory"
+            }
+        }
     }
-
-
+    
 }
+    
 
+class RandomNumber {
+    static func getRandomNumber(_ startOfRange: Int, _ endOfRange: Int) -> Int {
+        100
+    }
+}
