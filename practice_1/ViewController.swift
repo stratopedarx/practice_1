24cb66e24cd2bf6436a4ad6dc89f0e2c5b62ae1e
Sergey Lobanov <stratopedarx.lobanov@yandex.ru>
2021-06-24 22:53:05 +0200

    Add label to show the number of steps

--- practice_1/ViewController.swift
+++ practice_1/ViewController.swift
@@ -11,6 +11,7 @@ struct Message {
     static var victoryTitle = "victory_title"
     static var victoryMessage = "victory_message"
     static var startOver = "start_over"
+    static var steps = "steps"
 }
 
 
@@ -25,15 +26,23 @@ class ViewController: UIViewController {
     var computerNumber: Int!
     var startOfRange = 0
     var endOfRange = 100
+    var numberOfSteps = 0 {
+        didSet {
+            stepLabel.text = "\(Message.steps.localized()) : \(numberOfSteps)"
+        }
+    }
+    
     @IBOutlet weak var mainMessageLabel: UILabel!
     @IBOutlet weak var numberFromUserTextField: UITextField!
-
+    @IBOutlet weak var stepLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         mainMessageLabel.text = Message.welcome.localized()
     }
     
     @IBAction func getNumberFromUserAction(_ sender: UIButton) {
+        numberOfSteps += 1
         if let numberFromUser = Int(numberFromUserTextField.text!) {
             if computerNumber == nil {
                 computerNumber = RandomNumber.getRandomNumber(startOfRange, endOfRange)
@@ -57,6 +66,7 @@ class ViewController: UIViewController {
         let playAgainAlertAction = UIAlertAction(title: Message.startOver.localized(), style: .default) { action in
             self.mainMessageLabel.text = Message.welcomeAfterSuccessGame.localized()
             self.computerNumber = nil
+            self.numberOfSteps = 0
         }
         victoryAlert.addAction(playAgainAlertAction)
         self.present(victoryAlert, animated: true, completion: nil)
